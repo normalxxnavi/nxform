104f91d6d0b4d877f0fa1e49b035c6e7016edc46
Javier Lopez Velasco <javierlopezvelasco51@gmail.com>
2023-12-13 17:54:07 +0100

    formulario

--- src/app/actions.js
+++ src/app/actions.js
@@ -6,11 +6,43 @@ export async function handle(formData) {
     const nombre = formData.get('nombre')
     const apellidos = formData.get('apellidos')
     const avatar = formData.get('avatar')
+    const edad = formData.get('edad')
+    const telefono = formData.get('telefono')
+    const email = formData.get('email')
+    const password = formData.get('password')
+    const aficiones = formData.get('aficiones')
+    const id = formData.get('id')
+    const sexo = formData.get('sexo')
+    const observaciones = formData.get('observaciones')
+    const fecha = formData.get('fecha')
+    const mes = formData.get('mes')
+    const semana = formData.get('semana')
+    const rango = formData.get('rango')
+    const buscar = formData.get('buscar')
+    const url = formData.get('url')
     
     const buffer = await avatar.arrayBuffer()
     const bytes = new Uint8Array(buffer)
 
-    // console.log(nombre, apellidos, avatar)
+    console.group("Formulario Data");
+    console.log("Nombre:", nombre);
+    console.log("Apellidos:", apellidos);
+    console.log("Avatar:", avatar);
+    console.log("Edad:", edad);
+    console.log("Teléfono:", telefono);
+    console.log("Email:", email);
+    console.log("Contraseña:", password);
+    console.log("Aficiones:", aficiones);
+    console.log("Identificación:", id);
+    console.log("Sexo:", sexo);
+    console.log("Observaciones:", observaciones);
+    console.log("Fecha de Nacimiento:", fecha);
+    console.log("Mes:", mes);
+    console.log("Semana:", semana);
+    console.log("Rango:", rango);
+    console.log("Buscar:", buscar);
+    console.log("URL:", url);
+    console.groupEnd();
 
     await new Promise((resolve) => setTimeout( resolve, 1000))
 
--- src/app/formulario.jsx
+++ src/app/formulario.jsx
@@ -15,6 +15,40 @@ export function Formulario() {
         <form action={wrapper}>
             <input type="text" required name="nombre" placeholder="Introduce tu nombre" />
             <input type="text" required name="apellidos" placeholder="Introduce tus apellidos" />
+            <input type="number" required name="edad" placeholder="Introduzca su edad" />
+            <input type="tel" required name="telefono" placeholder="Introduce tu telefono" />
+            <input type="email" required name="email" placeholder="Introduce tu correo electronico" />
+            <input type="password" required name="password" placeholder="Introduce tu contraseña" />
+            <select required name="aficiones">
+                <option value="Programador">Programador</option>
+                <option value="Administrador de Red">Administrador de Red</option>
+                <option value="Otro">Otro</option>
+            </select>
+            <label>DNI</label>
+            <input type="radio" required name="id" placeholder="DNI" />
+            <label>NIF</label>
+            <input type="radio" required name="id" placeholder="NIF" />
+            <label>Pasaporte</label>
+            <input type="radio" required name="id" placeholder="Pasaporte" />
+            <label>Hombre</label>
+            <input type="checkbox" name="sexo" placeholder="Hombre" />
+            <label>Mujer</label>
+            <input type="checkbox" name="sexo" placeholder="Mujer" />
+            <label>Otro</label>
+            <input type="checkbox" name="sexo" placeholder="Otro" />
+            <label>Observaciones: </label>
+            <input type="textarea" required name="observaciones" placeholder="Observaciones" />
+            <label>Fecha de Naciemiento: </label>
+            <input type="date" required name="fecha" placeholder="Fecha" />
+            <label>Mes: </label>
+            <input type="month" required name="mes" placeholder="Mes" />
+            <label>Semana: </label>
+            <input type="week" required name="semana" placeholder="Semana" />
+            <label>Rango: </label>
+            <input type="range" required name="rango" placeholder="Rango" />
+            <label>Buscar: </label>
+            <input type="search" required name="buscar" />
+            <input type="url" required name="url" placeholder="Url" />
             <label htmlFor="avatar">
                 Selecciona un avatar para enviar al servidor
             </label>
